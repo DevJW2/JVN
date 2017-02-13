f6cdc6a0c8e533356adfc6027b11b6e58afc0954
Jeffrey Weng <jweng2@stuy.edu>
2017-02-13 12:33:12 -0500

    modified tester

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -1,8 +1,13 @@
+//Jeffrey Weng, Noah Tang, Victor Teoh
+//APCS2 pd3
+//HW07 -- What Does the Data Say?
+//2017-02-13
+
 /*======================================
   class MergeSortTester
 
   ALGORITHM:
-  <INSERT YOUR DISTILLATION OF ALGO HERE>
+  Mergesort Algorithm sorts a list by splitting it into sublists, by halving the array, until it reaches a length of 1, where we can then assume it's sorted. We then merge all the sorted smaller lists in ascending order until the array is the length of the original.
 
   BIG-OH CLASSIFICATION OF ALGORITHM:
   <INSERT YOUR EXECUTION TIME CATEGORIZATION OF MERGESORT HERE>
@@ -18,6 +23,7 @@
   ANALYSIS:
   <INSERT YOUR RESULTS ANALYSIS HERE>
   ======================================*/
+import java.lang.*;
 
 public class MergeSortTester 
 {
@@ -30,7 +36,16 @@ public class MergeSortTester
      ******************************/
     public static void main( String[] args ) 
     {
-
+	MergeSort test = new MergeSort();
+	int[] arr = new int[10];
+	for(int i = 0; i < arr.length; i++){
+	    arr[i] = (int)(Math.random() * (arr.length - 1)) + 1;
+	}
+	
+	long startTime = System.currentTimeMillis();
+	System.out.println(test.sort(arr));
+	long endTime = System.currentTimeMillis();
+	System.out.println("Total Time: " + (endTime - startTime));
     }//end main
 
 }//end class
