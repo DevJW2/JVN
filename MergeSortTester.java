748fab75548e4874016a626ee9040e0dca24b721
VictorTeoh <vteoh@stuy.edu>
2017-02-14 09:56:42 -0500

    slight comment change

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -10,13 +10,13 @@
   Mergesort Algorithm sorts a list by splitting it into sublists, by halving the array, until it reaches a length of 1, where we can then assume it's sorted. We then merge all the sorted smaller lists in ascending order until the array is the length of the original.
 
   BIG-OH CLASSIFICATION OF ALGORITHM:
-  <INSERT YOUR EXECUTION TIME CATEGORIZATION OF MERGESORT HERE>
-
+  nlog(n)
+  At each level 
   Mean execution times for dataset of size n:
   Batch size: <# of times each dataset size was run>
-  n=1       time: 0 milliseconds
-  n=10      time: 0 milliseconds
-  n=100     time: 0 milliseconds
+  n=1       time:  milliseconds
+  n=10      time:  milliseconds
+  n=100     time:  milliseconds
   n=1000    time: 1 milliseconds
   ...
   n=<huge>  time: > 3674 milliseconds at 10,000,000
